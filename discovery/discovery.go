f9d2d16e616a50137d3eddc17e5e8b343d02be52
Ernesto Alejo <ernestokarim@gmail.com>
2021-09-16 17:07:08 +0200

    discovery: Export internal code.

---
+++ discovery/discovery.go
@@ -0,0 +1,46 @@
+package discovery
+
+import (
+	"encoding/json"
+	"os"
+
+	"libs.altipla.consulting/env"
+	"libs.altipla.consulting/errors"
+	"libs.altipla.consulting/rdb"
+)
+
+func OpenRavenDB(dbname string, opts ...rdb.OpenOption) (*rdb.Database, error) {
+	var address string
+	if env.IsLocal() {
+		address = "http://localhost:13000" // development on the machines
+		if local := os.Getenv("LOCAL_RAVENDB"); local != "" {
+			address = local // development inside containers
+		}
+	}
+
+	if !env.IsLocal() {
+		var credentials struct {
+			Address string
+			Key     string
+			Cert    string
+			CACert  string
+		}
+		f, err := os.Open("/etc/secrets/ravendb-client-credentials")
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		defer f.Close()
+		if err := json.NewDecoder(f).Decode(&credentials); err != nil {
+			return nil, errors.Trace(err)
+		}
+		address = credentials.Address
+		opts = append(opts, rdb.WithSecurity(credentials.CACert, credentials.Key, credentials.Cert))
+	}
+
+	sess, err := rdb.Open(address, dbname, opts...)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	return sess, nil
+}
